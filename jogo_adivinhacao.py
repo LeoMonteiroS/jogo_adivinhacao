9f4b1ace64251a466735fe2e87ae23130f8cdb76
LeoMonteiroS <leeumonteiro@gmail.com>
2025-02-20 01:31:41 -0300

    jogo de adivinhação

---
+++ jogo_adivinhacao.py
@@ -0,0 +1,27 @@
+import random
+
+numero_secreto = random.randint(1, 100)
+chances = 0
+
+print('Bem-Vindo ao jogo de Adivinhação! ')
+print('-'*30)
+print('Tente adivinhar o numero que estou pensando ')
+print('-'*30)
+
+while True:
+    tentativa = int(input('Digite um numero de 1 a 100 para adivinhar: '))
+    chances += 1
+
+    if tentativa == numero_secreto:
+        print(f'PARABENS! VOÇÊ ACERTOU EM {chances} tentativa(s) ')
+        break
+
+    elif tentativa < numero_secreto:
+         print('O numero secreto e maior, tente novamente ')
+         print('-'*30)
+
+    elif tentativa > numero_secreto:
+            print('O numero secreto e menor, tente novamente ')
+            print('-'*30)
+
+    
